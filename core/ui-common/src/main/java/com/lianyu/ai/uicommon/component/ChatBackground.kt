8ae804995a648472610401dc870967ce7b0cad22
tjfgcu-prog <tjfgcu@gmail.com>
2026-08-08 17:32:37 +0800

    Update ChatBackground.kt

--- core/ui-common/src/main/java/com/lianyu/ai/uicommon/component/ChatBackground.kt
+++ core/ui-common/src/main/java/com/lianyu/ai/uicommon/component/ChatBackground.kt
@@ -29,9 +29,9 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
