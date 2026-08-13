b68fc1f0c069f427ff8b977f9e139d327931f950
tjfgcu-prog <tjfgcu@gmail.com>
2026-08-13 11:28:21 +0800

    Update MainScreen.kt

--- app/src/main/java/com/lianyu/ai/MainScreen.kt
+++ app/src/main/java/com/lianyu/ai/MainScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
+
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.ExperimentalFoundationApi
